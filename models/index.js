7296d143927ade05e96ff8cc00343cb66a6b9c73
Dave Estey <daveestey.working@gmail.com>
2023-02-05 20:00:42 -0400

    poplated the Models and model>index file

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo()
+Product.belongsTo(Category, {
+  foreignKey: "catagory_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "catagory_id",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_tag",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "product_tag"
+});
 module.exports = {
   Product,
   Category,
