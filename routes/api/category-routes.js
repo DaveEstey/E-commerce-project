0078b17ea8e050f065681afe760411847093529d
Dave Estey <daveestey.working@gmail.com>
2023-02-06 23:40:37 -0400

    Added all delete routes and got the change product to work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,8 +50,19 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const oneCategory = await Category.destroy({ where : { id: req.params.id } })
+  
+    if (!oneCategory) {
+      res.status(404).json({ message: "No Category found with that id!" });
+      return;
+    }
+    res.status(200).json({ message: "Category deleted" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
